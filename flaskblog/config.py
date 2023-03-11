1e97bdad80e68c1a619d79f9e8a38a4cc068733b
nbodiwala <nbodiwala@gmail.com>
2023-03-11 09:48:39 -0600

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '356486a8c2208e0aa109106388986307'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TILS = True
+    MAIL_USERNAME = 'nbodiwala@gmail.com'
+    MAIL_PASSWORD = 'oZr##O1FI!3i*69$'
